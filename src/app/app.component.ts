ef0fd2905bf6452e42ec3e83fc3fb5a9b049ba0f
Antonina <tosia.h@onet.pl>
2020-05-20 16:19:22 +0200

    zadanie 1 i 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'codeBreakGame-app';
+
+
+  toDoList = [{name: 'Trololo', isFinished: false}, {name: 'Kacperiusz', isFinished: true}];
+  currentItem = '';
+
+  number = 5;
+
+   get isNumberPositive(): boolean {
+     return this.number > 0;
+   }
+
+   get numberValue(): number {
+     return this.number;
+   }
+   set numberValue(numberValue: number) {
+     if (numberValue < 10) {
+       this.number = numberValue;
+     }
+   }
+
+  addElement() {
+     console.log({name: this.currentItem, isFinished: false});
+     console.log(this.toDoList);
+     if (this.currentItem) {
+       this.toDoList.push({name: this.currentItem, isFinished: false});
+     }
+  }
+
+  incrementValue() {
+    this.numberValue += 1;
+  }
+  decrementValue() {
+    this.numberValue -= 1;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import { GuessComponent } from './guess/guess.component';
+import { CipherComponent } from './cipher/cipher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuessComponent,
+    CipherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
